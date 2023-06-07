168c74358095a400abd70e648a1217c1839ccc43
Qmo <qmo.yt@medialand.tw>
2023-06-07 11:35:17 +0800

    setup 1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
-import virtualHtml from 'vite-plugin-virtual-html'
+import ejs from 'ejs'
+import pugPlugin from "vite-plugin-pug"
+import VirtualHtml from 'vite-plugin-virtual-html'
 
-const pages = {
-  demo: '/src/pages/index.html',
-  about: '/src/pages/about.html',
-  test: '/src/pages/test.html',
-}
+const pageList = [
+  // index 為預設頁面, 不能移除
+  { path: 'index', title: 'demo', pug: 'demo', script: '' },
+  { path: 'about', title: 'about', pug: 'about', script: 'about' },
+  { path: 'test', title: 'test', pug: 'test', script: '' },
+]
 
-// https://vitejs.dev/config/
+const pages = {};
+pageList.forEach((item) => {
+  pages[item.path] = {
+    // 預設樣板
+    template: '/src/pages/index.html',
+    data: {
+      title: item.title,
+      pug: `<pug src="src/pages/${item.pug}.pug"></pug>`,
+      script: item.script && `<script type="module" src="src/js/${item.script}.js"></script>`
+    }
+  }
+})
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -16,9 +31,12 @@ export default defineConfig({
     }
   },
   plugins: [
-    virtualHtml({
+    pugPlugin(),
+    VirtualHtml({
       pages,
-      indexPage: 'demo'
+      render(template, data){
+        return ejs.render(template, data, { delimiter: '%', root: process.cwd() })
+      },
     })
   ],
 })
